f817cfea31695ae4997be16ab5c28a58344d2594
william-parker <edwinious@gmail.com>
2019-04-19 11:42:27 -0400

    Sprint Challenge - Arrays - DONE

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -76,7 +76,15 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
-console.log(universities)
+for(let i = 0; i < graduates.length; i++){
+  universities.push(graduates[i].university);
+}
+console.log(universities.sort());
+
+/*****************************************************************
+*****        DONE ABOVE                                           
+*****************************************************************/
+
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
 
@@ -85,13 +93,27 @@ Name email@example.com
 
 Log the result of your new array. */
 const contactInfo = [];
+for(let i = 0; i < graduates.length; i++){
+  contactInfo.push(graduates[i].first_name + " " + graduates[i].email)
+};
 console.log(contactInfo);
 
+/*****************************************************************
+*****        DONE ABOVE                                           
+*****************************************************************/
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
 const uni = [];
+for(let i = 0; i < graduates.length; i++){
+  if(graduates[i].university.includes("Uni")){
+    uni.push(graduates[i].university);
+  }
+}
 console.log(uni);
 
+/*****************************************************************
+*****        DONE ABOVE                                           
+*****************************************************************/
 
 // ==== ADVANCED Array Methods ====
 
